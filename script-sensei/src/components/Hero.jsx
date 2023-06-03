c8ec4a5e503cdb7b0f44af460a57e50eb09b0cdb
SurajRajbhar <rajbharsuraj555@gmail.com>
2023-06-03 14:19:12 +0530

    info

--- script-sensei/src/components/Hero.jsx
+++ script-sensei/src/components/Hero.jsx
@@ -6,6 +6,10 @@ const Hero = () => {
         <div>
          <h2 class="text-center text-5xl font-bold">Script Sensie</h2>
             <h3>Motto</h3>
+            <h3 class="box-border h-32 w-3/4 p-4 border-4 text-center">Use long descriptive titles
+Go through every step to ensure your content stays relevant
+Outlines that are long and more specific generate the most relevant content
+Use supercharge writing quality to produce more sophisticated content, deeper arguments, and improved in text references</h3>
             <h4>Tagline</h4>
         </div>
     </div>
