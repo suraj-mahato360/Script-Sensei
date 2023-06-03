a18dd4d5be361454e36af0af8f24c4367dab04ec
SurajRajbhar <rajbharsuraj555@gmail.com>
2023-06-03 14:13:19 +0530

    info

--- script-sensei/src/components/Hero.jsx
+++ script-sensei/src/components/Hero.jsx
@@ -4,8 +4,9 @@ const Hero = () => {
   return (
     <div>
         <div>
-         <h2 class="text-center text-5xl font-bold">Script Sensie</h2>
+         <h2 class="text-center text-5xl font-bold">Script Sensei</h2>
             <h3>Motto</h3>
+            <h3 class="box-border h-32 w-2/4 p-4 border-4">Our AI Descriptive Essay Writer is designed to provide well structured essays about various topics written in a descriptive format. They provide an excellent base for any essay paper or even articles</h3>
             <h4>Tagline</h4>
         </div>
     </div>
