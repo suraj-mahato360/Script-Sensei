eabae6045a304cc20b04390b3b1f7d52ec44c267
sushan4 <uchilsushan36@gmail.com>
2023-06-03 14:23:26 +0530

    Added template

--- script-sensei/src/components/Hero.jsx
+++ script-sensei/src/components/Hero.jsx
@@ -3,15 +3,59 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
-        <div>
-         <h2 class="text-center text-5xl font-bold">Script Sensie</h2>
-            <h3>Motto</h3>
-            <h3 class="box-border h-32 w-3/4 p-4 border-4 text-center">Use long descriptive titles
-Go through every step to ensure your content stays relevant
-Outlines that are long and more specific generate the most relevant content
-Use supercharge writing quality to produce more sophisticated content, deeper arguments, and improved in text references</h3>
-            <h4>Tagline</h4>
-        </div>
+      <div>
+        <section class="bg-white dark:bg-gray-900">
+          <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div class="mr-auto place-self-center lg:col-span-7">
+              <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+                ScriptSensei
+              </h1>
+              <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                Empowering Your Imagination!
+              </p>
+              <a
+                href="#"
+                class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+              >
+                Get started
+                <svg
+                  class="w-5 h-5 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+              <a
+                href="#"
+                class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              >
+                Speak to Sales
+              </a>
+            </div>
+            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+              <img
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
+                alt="mockup"
+              ></img>
+            </div>
+          </div>
+        </section>
+        <h3>Motto</h3>
+        <h3 class="box-border h-32 w-3/4 p-4 border-4 text-center">
+          Use long descriptive titles Go through every step to ensure your
+          content stays relevant Outlines that are long and more specific
+          generate the most relevant content Use supercharge writing quality to
+          produce more sophisticated content, deeper arguments, and improved in
+          text references
+        </h3>
+        <h4>Tagline</h4>
+      </div>
     </div>
   );
 };
