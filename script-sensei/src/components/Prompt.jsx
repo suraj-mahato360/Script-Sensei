120d081b2bdb7b3f4dd957e6d83837e5ba8adb2a
Sushan Uchil <88882430+sushan4@users.noreply.github.com>
2023-06-03 13:05:10 +0530

    added genres

--- script-sensei/src/components/Prompt.jsx
+++ script-sensei/src/components/Prompt.jsx
@@ -17,7 +17,11 @@ const Prompt = () => {
                     <option value="suspence">Suspense</option>
                     <option value="thriller">Thriller</option>
                     <option value="romance">Romance</option>
-                    <option value=""></option>
+                    <option value="drama">Drama</option>
+                    <option value="scifi">Science Fiction</option>
+                    <option value="horror">Horror</option>
+                    <option value="mystery">Mystery</option>
+                    <option value="fantasy">Fantasy</option>
                 </select>
                 <input type="" />
             </div>
