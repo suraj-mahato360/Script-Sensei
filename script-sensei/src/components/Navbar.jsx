3e56ce417f6669ca069b99e5fe475da3d8c08eed
sushan4 <uchilsushan36@gmail.com>
2023-06-03 13:53:16 +0530

    name update

--- script-sensei/src/components/Navbar.jsx
+++ script-sensei/src/components/Navbar.jsx
@@ -1,22 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 py-4 px-6 flex items-center justify-between">
       <div className="flex items-center">
-        <img
-          src="#"
-          alt="Logo"
-          className="h-8 w-8 mr-2"
-        />
-        <h1 className="text-white text-lg font-semibold">Web App Title</h1>
+        <img src="#" alt="Logo" className="h-8 w-8 mr-2" />
+        <h1 className="text-white text-lg font-semibold">ScriptSensei</h1>
       </div>
       <div className="flex items-center">
-        <img
-          src="#"
-          alt="Avatar"
-          className="h-8 w-8 rounded-full"
-        />
+        <img src="#" alt="Avatar" className="h-8 w-8 rounded-full" />
         <span className="text-white ml-2">Username</span>
       </div>
     </nav>
